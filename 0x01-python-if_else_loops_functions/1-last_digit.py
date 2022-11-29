2596650babb4f1d4a6b0833de3238f95649d463e
kelvin <edithmukoro3@gmail.com>
2022-11-30 00:37:08 +0100

    using if statement to detect a negative or positive number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,12 @@
-#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = 0
-
-if number >= 0:
-	digit = number % 10
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+print("Last digit of {} is {} and is ".format(number, digit), end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
 else:
-	digit = (-number % 10) * -1
-
-txt = f"Last digit of {number} is {digit}"
-
-if digit == 0:
-	print(f"{txt} and is 0")
-elif digit > 5 and digit % 10 != 0:
-	print(f"{txt} and is greater than 5")
-else:
-	print(f"{txt} and is less than 6 and not 0")
+    print("less than 6 and not 0")
