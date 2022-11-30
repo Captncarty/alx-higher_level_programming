3086c865e3d08b2505fbad0ca9e9b51a79ba0b11
kelvin <edithmukoro3@gmail.com>
2022-11-30 01:15:40 +0100

    minor fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-"""Print numbers 0 to 98 in decimal and hexadecimal."""
-for number in range(0, 99):
-    print("{} = {}".format(number, hex(number)))
+for num in range(0, 100):
+    if num != 99:
+        print("{:02d}, ".format(num), end='')
+    else:
+        print("{:02d}".format(num))
