d94450527471b7cef583d7a4cbe4ca839e3c596e
kelvin <edithmukoro3@gmail.com>
2022-12-08 02:48:00 +0100

    err fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
-    max_val = 0
-    winner = None
-    if type(a_dictionary) is dict:
-        for key, value in a_dictionary.items():
-            if value > max_val:
-                max_val = value
-                winner = key
-    return 
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
