e868e5a16bc1affda965d57cb6bce358447231de
kelvin <edithmukoro3@gmail.com>
2023-01-03 00:44:23 +0100

    First commit
    An Empty Class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Created on Tue Jan 3 2023
+@Author: Kelvin Maccarthy
+"""
+
+class Rectangle:
+	""" An empty class that defines a rectangle
+
+	Attributes:
+		empty
+	"""
+	pass
