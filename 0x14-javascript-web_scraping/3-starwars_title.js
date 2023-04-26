acfb265cf52600fcb06752dc056ed151be6a60af
kelvin <edithmukoro3@gmail.com>
2023-04-26 01:22:34 +0000

    Task_04

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,13 +5,13 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 const episode = process.argv[2];
 
-request(url + episode, function(err, response, body) {
-   if (err) {
+request(url + episode, function (err, response, body) {
+  if (err) {
     throw err;
-   } else if (response.statusCode === 200) {
+  } else if (response.statusCode === 200) {
     const load = JSON.parse(body);
     console.log(load.title);
-   } else {
-    console.log("error:", response.statusCode);
-   }
+  } else {
+    console.log('error:', response.statusCode);
+  }
 });
