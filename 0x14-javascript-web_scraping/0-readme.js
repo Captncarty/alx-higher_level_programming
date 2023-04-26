42d50382cc3c21b7bed43f157b9972a5428dc71b
kelvin <edithmukoro3@gmail.com>
2023-04-26 00:27:04 +0000

    Task_01

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-/* Write a script that reads and prints the content of a file.*/
+/* Write a script that reads and prints the content of a file. */
 
 const fs = require('fs');
-file = process.argv[2];
-fs.readFile(file, 'utf-8', function(err, content) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(content);
-    }
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', function (err, content) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
 });
